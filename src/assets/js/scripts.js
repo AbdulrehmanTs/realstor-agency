f5a72a2150816035c926ecb6d51c8f3cafecf65b
AbdulrehmanTs <rohi644@gmail.com>
2024-09-02 20:46:03 +0500

    sidebar nav added

--- src/assets/js/scripts.js
+++ src/assets/js/scripts.js
@@ -1,3 +1,7 @@
+const navBtn = document.getElementById("nav-btn")
+const navbar = document.getElementById("navbar")
+const navitemTitles = document.getElementsByClassName("navitem-title")
+
 const map = document.getElementById("map");
 const grid = document.getElementById("grid");
 const gridCards = document.getElementsByClassName("grid-card");
@@ -9,6 +13,7 @@ const currenciesContainer = document.getElementById("currencies");
 const leftSidebar = document.getElementById("left-sidebar");
 const mainContainer = document.getElementById("main-container")
 
+navBtn.addEventListener("click", toggleNavbar)
 currencyButton.addEventListener("click", handleCurrencyChange);
 mapSwitch.addEventListener("click", handleMapswitch);
 gridSwitch.addEventListener("click", handleGridswitch);
@@ -16,6 +21,26 @@ map.addEventListener("click", openLeftSidebar);
 gridCardsArray.forEach((item) =>
   item.addEventListener("click", openLeftSidebar)
 );
+
+function toggleNavbar () {
+  if (navbar.classList.contains("max-w-16")) {
+    navbar.classList.remove("max-w-16")
+    navbar.classList.add("max-w-52")
+    navBtn.firstElementChild.setAttribute("name", "chevron-back")
+    setTimeout(()=> {
+      Array.from(navitemTitles).forEach(item=> {
+        item.classList.remove("hidden")
+      })
+    }, 150)
+  } else {
+    navbar.classList.remove("max-w-52")
+    navbar.classList.add("max-w-16")
+    navBtn.firstElementChild.setAttribute("name", "reorder-three")
+    Array.from(navitemTitles).forEach(item=> {
+      item.classList.add("hidden")
+    })
+  }
+}
 
 function handleCurrencyChange() {
   if (currenciesContainer.classList.contains("scale-0")) {
@@ -28,23 +53,32 @@ function handleCurrencyChange() {
 }
 
 function handleMapswitch() {
-  if (mapSwitch.classList.contains("bg-gray-200")) {
+  if (mapSwitch.classList.contains("bg-blue-500")) {
     return;
   } else {
-    mapSwitch.classList.add("bg-gray-200", "shadow-sm");
-    gridSwitch.classList.remove("bg-gray-200", "shadow-sm");
+    mapSwitch.classList.add("bg-blue-500", "shadow-sm");
+    gridSwitch.classList.remove("bg-blue-500", "shadow-sm");
+    mapSwitch.firstElementChild.classList.remove("text-gray-600")
+    mapSwitch.firstElementChild.classList.add("text-white")
+    gridSwitch.firstElementChild.classList.remove("text-white")
+    gridSwitch.firstElementChild.classList.add("text-gray-600")
     if (map.classList.contains("hidden")) {
       map.classList.remove("hidden");
       grid.classList.add("hidden");
     }
   }
 }
+
 function handleGridswitch() {
-  if (gridSwitch.classList.contains("bg-gray-200")) {
+  if (gridSwitch.classList.contains("bg-blue-500")) {
     return;
   } else {
-    gridSwitch.classList.add("bg-gray-200", "shadow-sm");
-    mapSwitch.classList.remove("bg-gray-200", "shadow-sm");
+    gridSwitch.classList.add("bg-blue-500", "shadow-sm");
+    mapSwitch.classList.remove("bg-blue-500", "shadow-sm");
+    gridSwitch.firstElementChild.classList.remove("text-gray-600")
+    gridSwitch.firstElementChild.classList.add("text-white")
+    mapSwitch.firstElementChild.classList.remove("text-white")
+    mapSwitch.firstElementChild.classList.add("text-gray-600")
     if (grid.classList.contains("hidden")) {
       grid.classList.remove("hidden");
       map.classList.add("hidden");
